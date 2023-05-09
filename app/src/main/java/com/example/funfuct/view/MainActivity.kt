518ce1060d3b366bd50cb823e71fea984a429161
farida <farida.hairullina5@gmail.com>
2023-05-09 19:37:35 +0200

    initial commit

--- app/src/main/java/com/example/funfuct/view/MainActivity.kt
+++ app/src/main/java/com/example/funfuct/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.funfuct
+package com.example.funfuct.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.funfuct.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
