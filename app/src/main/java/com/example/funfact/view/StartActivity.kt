c29110e31a23b2f08075f3e737f62ff17b22a2bd
farida <farida.hairullina5@gmail.com>
2023-05-11 19:47:40 +0200

    webview in progress

--- app/src/main/java/com/example/funfact/view/StartActivity.kt
+++ app/src/main/java/com/example/funfact/view/StartActivity.kt
@@ -1,15 +1,19 @@
 package com.example.funfact.view
 
 import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.opengl.Visibility
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.webkit.CookieManager
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.funfact.BuildConfig
 import com.example.funfact.R
 import com.example.funfact.databinding.ActivityStartBinding
@@ -17,6 +21,7 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import java.net.InetAddress
 import java.util.*
 
 class StartActivity : AppCompatActivity() {
@@ -26,7 +31,8 @@ class StartActivity : AppCompatActivity() {
     //private val firebaseRemoteConfig: FirebaseRemoteConfig
     private val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("SetJavaScriptEnabled", "CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
@@ -38,27 +44,27 @@ class StartActivity : AppCompatActivity() {
         remoteConfig.setConfigSettingsAsync(configSettings)
         remoteConfig.setDefaultsAsync(R.xml.url_default_value)
         getValueFromFireBaseRemoteConfig()
-        checkIsEmu()
 
         setContentView(binding.root)
+
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) binding.webView.goBack()
+        else super.onBackPressed()
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun getValueFromFireBaseRemoteConfig() {
         remoteConfig.fetchAndActivate().addOnCompleteListener(this) { task ->
             if (task.isSuccessful) {
-                //val updated = task.result
                 val urlText = remoteConfig.getString("url")
-                Log.d(TAG, "Config params updated: $urlText")
-                Toast.makeText(
-                    this,
-                    "Fetch and activate succeeded",
-                    Toast.LENGTH_SHORT,
-                ).show()
-                if (urlText.isNotEmpty()) {
+                if (urlText.isEmpty() || checkIsEmu()) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
                     with(binding.webView) {
                         webViewClient = WebViewClient()
-                        loadUrl("$urlText")
+                        loadUrl(urlText)
                         settings.javaScriptEnabled = true
                         settings.domStorageEnabled = true
                         settings.javaScriptCanOpenWindowsAutomatically = true
@@ -69,8 +75,6 @@ class StartActivity : AppCompatActivity() {
                         settings.allowFileAccess = true
                         settings.allowContentAccess = true
                     }
-                } else {
-                    startActivity(Intent(this,MainActivity::class.java))
                 }
             } else {
                 Toast.makeText(
@@ -82,40 +86,44 @@ class StartActivity : AppCompatActivity() {
         }
     }
 
+    private fun isInternetAvailable(): Boolean {
+        return try {
+            val ipAddr: InetAddress = InetAddress.getByName("https://google.com")
+            //You can replace it with your name
+            !ipAddr.equals("")
+        } catch (e: Exception) {
+            false
+        }
+    }
+
     private fun checkIsEmu(): Boolean {
         if (BuildConfig.DEBUG) return false // when developer use this build on emulator
         val phoneModel = Build.MODEL
         val buildProduct = Build.PRODUCT
         val buildHardware = Build.HARDWARE
-        val brand: String = Build.BRAND;
+        val brand: String = Build.BRAND
         var result = (Build.FINGERPRINT.startsWith("generic")
-                    || phoneModel.contains("google_sdk")
-                    || phoneModel.lowercase(Locale.getDefault()).contains("droid4x")
-                    || phoneModel.contains("Emulator")
-                    || phoneModel.contains("Android SDK built for x86")
-                    || Build.MANUFACTURER.contains("Genymotion")
-                    || buildHardware == "goldfish"
-                    || Build.BRAND.contains("google")
-                    || buildHardware == "vbox86"
-                    || buildProduct == "sdk"
-                    || buildProduct == "google_sdk"
-                    || buildProduct == "sdk_x86"
-                    || buildProduct == "vbox86p"
-                    || Build.BOARD.lowercase(Locale.getDefault()).contains("nox")
-                    || Build.BOOTLOADER.lowercase(Locale.getDefault()).contains("nox")
-                    || buildHardware.lowercase(Locale.getDefault()).contains("nox")
-                    || buildProduct.lowercase(Locale.getDefault()).contains("nox"))
+                || phoneModel.contains("google_sdk")
+                || phoneModel.lowercase(Locale.getDefault()).contains("droid4x")
+                || phoneModel.contains("Emulator")
+                || phoneModel.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || buildHardware == "goldfish"
+                || Build.BRAND.contains("google")
+                || buildHardware == "vbox86"
+                || buildProduct == "sdk"
+                || buildProduct == "google_sdk"
+                || buildProduct == "sdk_x86"
+                || buildProduct == "vbox86p"
+                || Build.BOARD.lowercase(Locale.getDefault()).contains("nox")
+                || Build.BOOTLOADER.lowercase(Locale.getDefault()).contains("nox")
+                || buildHardware.lowercase(Locale.getDefault()).contains("nox")
+                || buildProduct.lowercase(Locale.getDefault()).contains("nox"))
 
         if (result) return true
-        result = result or (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+        result = result or (brand.startsWith("generic") && Build.DEVICE.startsWith("generic"))
         if (result) return true
         result = result or ("google_sdk" == buildProduct)
         return result
-    }
-
-
-    override fun onBackPressed() {
-        if (binding.webView.canGoBack()) binding.webView.goBack()
-        else super.onBackPressed()
     }
 }
